511035e57646be01b1c08aabfed93d6e35ca0005
gabicilfone <cilfonegabriel@gmail.com>
2023-04-05 14:14:37 -0300

    we change the user_id data model to author_id

--- db/migrate/20230405170459_create_user.rb
+++ db/migrate/20230405170459_create_user.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
@@ -12,3 +10,4 @@ class CreateUser < ActiveRecord::Migration[7.0]
     end
   end
 end
+
--- db/migrate/20230405170903_create_comment.rb
+++ db/migrate/20230405170903_create_comment.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }
       t.references :post, null: false, foreign_key: true
       t.text :text
 
