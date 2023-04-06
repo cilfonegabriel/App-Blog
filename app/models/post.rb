1a1e9302bcfd6327fbe626331143d2dad03e5581
gabicilfone <cilfonegabriel@gmail.com>
2023-04-05 21:12:16 -0300

    add methods to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,17 @@
 class Post < ApplicationRecord
+    has_many :comments, foreign_key: 'post_id', dependent: :destroy
+    has_many :likes, foreign_key: 'post_id', dependent: :destroy
     belongs_to :author, class_name: 'User'
-    has_many :likes
-    has_many :comments
-end
+  
+    def recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+  
+    after_save :update_post_counter
+  
+    private
+  
+    def update_post_counter
+      author.update(posts_counter: author.posts.count)
+    end
+  end
