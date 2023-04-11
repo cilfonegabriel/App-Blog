8ae13531642cd74f9b8af3beab180b9c793d8ad6
gabicilfone <cilfonegabriel@gmail.com>
2023-04-11 15:52:55 -0300

    add html and install gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
     def index
+        @user = User.find(params[:user_id])
         @posts = @user.posts
-        @user = User.find(params[:user_id])
-    end
+      end
 
-    def show
-        @post = @user.posts.find(params[:id])
+      def show
         @user = User.find(params[:user_id])
-    end
+        @post = @user.posts.find(params[:id])
+      end
 end
