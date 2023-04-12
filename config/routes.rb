ea72e6fa6cb022fb2c23c7162cb38ab52a59e5e7
gabicilfone <cilfonegabriel@gmail.com>
2023-04-12 13:57:53 -0300

    delete a line

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
     resources :posts, only: [:index, :show, :create, :new] do
       resources :likes, only: [:new, :create]
       resources :comments, only: [:new, :create]
