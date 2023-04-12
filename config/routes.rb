89c9238984ba4d4ce6396645204000293cec5971
gabicilfone <cilfonegabriel@gmail.com>
2023-04-12 13:51:05 -0300

    changes in the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  resources :users do
-    resources :posts do
-      resources :comments, :likes
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create, :new] do
+      resources :likes, only: [:new, :create]
+      resources :comments, only: [:new, :create]
     end
   end
 end
