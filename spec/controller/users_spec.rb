70bc95d849feba0a6e6a70ff66ba20721134c17c
gabicilfone <cilfonegabriel@gmail.com>
2023-04-17 17:30:21 -0300

    add test for user controller test

--- spec/controller/users_spec.rb
+++ spec/controller/users_spec.rb
@@ -1,25 +1,45 @@
 require 'rails_helper'
-
-RSpec.describe UsersController, type: :request do
-  describe 'Get id' do
-    it 'returns a successful response' do
-      get users_path
-      expect(response).to be_successful
+RSpec.describe 'Users', type: :request do
+  before(:each) do
+    @user1 = User.create(
+      name: 'User 1',
+      photo: 'https://unsplash.com/photos/Hekapoo',
+      bio: 'User 1 bio',
+      posts_counter: 0
+      )
     end
 
-    it 'renders the index template' do
+    describe 'GET /index' do
+    it 'returns http success' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns render index template' do
       get users_path
       expect(response).to render_template(:index)
     end
 
-    it 'renders the show template' do
-      get '/users/:id'
+    it 'body includes correct placeholder text' do
+      get users_path
+      expect(response.body).to include(@user1.name)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get users_path(@user1)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns render show template' do
+      get user_path(@user1)
       expect(response).to render_template(:show)
     end
 
-    it ' includes the correct placeholder text in the response body' do
-      get '/users/'
-      expect(response.body).to include('Users#index')
+    it 'body includes correct photo text' do
+      get users_path
+      expect(response.body).to include(@user1.name)
     end
   end
 end
