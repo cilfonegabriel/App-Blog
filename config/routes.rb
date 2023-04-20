4566a37aa76fdfc79f65c426d3ffaff299ac0830
gabicilfone <cilfonegabriel@gmail.com>
2023-04-20 15:09:51 -0300

    Define routes for API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
       resources :comments, only: [:new, :create, :destroy]
     end
   end
+  resources :api do
+    resources :users do
+      resources :posts do
+        resources :comments
+      end
+    end
+  end
 end
